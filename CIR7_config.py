5cd3d129c6d455495225222b8e01778effe8ae96
Baptiste Jadot <jadot.baptiste@gmail.com>
2021-08-25 10:26:48 +0200

    Updated CIR7 Driver with fastseq

---
+++ CIR7_config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 13:15:56 2021
+
+@author: manip.batm
+"""
+
+#############
+## DC DACS
+#############
+DAC_dict = {}
+
+DAC_dict['VE0N'] = {'panel':0, 'channel':1, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['VE1N'] = {'panel':0, 'channel':3, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['MODE_ROT'] = {'panel':0, 'channel':4, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['SEL_MAT'] = {'panel':0, 'channel':5, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['OSC_VCO'] = {'panel':0, 'channel':6, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['RESETN'] = {'panel':0, 'channel':7, 'lower_limit':0., 'upper_limit':1.8}
+
+DAC_dict['VBGPA'] = {'panel':3, 'channel':0, 'lower_limit':-3., 'upper_limit':0.}
+DAC_dict['VBGNA'] = {'panel':3, 'channel':1, 'lower_limit':0., 'upper_limit':3.}
+DAC_dict['VBGNC'] = {'panel':3, 'channel':2, 'lower_limit':0., 'upper_limit':3.}
+DAC_dict['VBGPC'] = {'panel':3, 'channel':3, 'lower_limit':-3., 'upper_limit':0.}
+DAC_dict['VBGNM'] = {'panel':3, 'channel':4, 'lower_limit':0., 'upper_limit':3.}
+DAC_dict['VBGPM'] = {'panel':3, 'channel':5, 'lower_limit':-3., 'upper_limit':0.}
+
+DAC_dict['CMD_R0'] = {'panel':2, 'channel':0, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['CMD_R1'] = {'panel':2, 'channel':1, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['CMD_R2'] = {'panel':2, 'channel':2, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['CMD_R3'] = {'panel':2, 'channel':3, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['CMD_R4'] = {'panel':2, 'channel':4, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['CMD_R5'] = {'panel':2, 'channel':5, 'lower_limit':0., 'upper_limit':1.8}
+DAC_dict['CMD_R6'] = {'panel':2, 'channel':6, 'lower_limit':0., 'upper_limit':1.8}
+
--- CIR7_driver.py
+++ CIR7_driver.py
@@ -1,34 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 12 09:44:21 2021
+Created on Tue Aug 24 09:33:07 2021
 
-@author: manip.batm
+@author: Baptiste
 """
 
 import numpy as np
 from nifpga import Session, FpgaViState
-from nifpga.status import RpcConnectionErrorError, FpgaBusyInteractiveError
-from FPGA_utils import build_order,uint16_to_float,float_to_uint16
-# from SPI_driver import SPI_driver
+from nifpga.status import RpcConnectionErrorError, FpgaBusyInteractiveError, FifoTimeoutError
+from FPGA_utils import build_order, uint16_to_float, float_to_uint16
 import time
 
 class CIR7_driver():
     """
     Main driver for CIR7 circuit v2.0
     """
-    def __init__(self,ip_address,bitfile_path,DAC_dict={}):
+
+######################################
+###      FPGA COMMUNICATION
+######################################
+
+    def __init__(self, ip_address, bitfile_path, DAC_dict={}):
         """
         Opens communication and finds the VI objects to control
         DAC_dict is a dictionnary of each DAC output (with fields 'panel', 'channel', 'lower_limit' and 'upper_limit')
         """
         self.ip_address = ip_address
         if DAC_dict == {}:
-            self.DAC_dict = {str(i)+':'+str(j):{'panel':i,'channel':j,'lower_limit':-5.,'upper_limit':5.} for i in range(8) for j in range(8)} # default '0:3': {0,3,-5V,5V}]
+            self.DAC_dict = {f"{i}:{j}" : {'panel':i, 'channel':j, 'lower_limit':-5., 'upper_limit':5.} for i in range(8) for j in range(8)} # default '0:3': {0,3,-5V,5V}]
         else:
             self.DAC_dict = DAC_dict
         try:
-            self.FPGA = Session(bitfile=bitfile_path,resource='rio://'+ip_address+'/RIO0',no_run=True,reset_if_last_session_on_exit=False)
-            if self.FPGA.fpga_vi_state!=FpgaViState(2): # FPGA is not running
+            self.FPGA = Session(bitfile = bitfile_path, resource = 'rio://' + ip_address + '/RIO0', no_run=False, reset_if_last_session_on_exit=False)
+            if self.FPGA.fpga_vi_state != FpgaViState(2): # FPGA is not running
                 self.FPGA.run()
         except RpcConnectionErrorError:
             raise RuntimeError('FPGA not found')
@@ -51,26 +55,31 @@ class CIR7_driver():
             
             # configure communication speed
             config_orders = []
-            config_orders += [build_order([(2,8),(4,8),(0,8*2),(1600,32)])] # SPI speed
+            config_orders += [build_order([(2, 8), (4, 8), (0, 16), (1600, 32)])] # SPI speed
             self.send_orders(config_orders)
     
-    def send_orders(self,orders):
+    def send_orders(self, orders):
         """
         Puts orders in the FPGA input buffer and waits for until they are all passed.
         orders is a list of uint64s (see FPGA doc).
         """
         #print(orders)
-        while len(orders)>0:
-            self.FPGA.orders_fifo.write(orders[:500],timeout_ms=10) # FIFO size is 1050 elements
+        while len(orders) > 0:
+            self.FPGA.orders_fifo.write(orders[:500], timeout_ms=10) # FIFO size is 1050 elements
             orders = orders[500:]
-            irq_status = self.FPGA.wait_on_irqs(0,timeout_ms=10)
+            irq_status = self.FPGA.wait_on_irqs(0, timeout_ms=10)
             if irq_status.timed_out:
                 raise RuntimeError('Timeout while passing orders')
+
+######################################
+###            DC DACS
+######################################
     
-    def update_DAC(self,values):
+    def update_DAC(self, values):
         """
         Updates one or several DAC output(s) to the indicated value(s).
-        values is a dict of type {DAC_key1:val1,DAC_key2:val2}
+        values is a dict of type {DAC_key1:val1, DAC_key2:val2}.
+        Direct jump to value for CIR7.
         """
         orders = []
         for (key, val) in values.items():
@@ -81,171 +90,200 @@ class CIR7_driver():
                 chan = self.DAC_dict[key]['channel']
                 ll = self.DAC_dict[key]['lower_limit']
                 ul = self.DAC_dict[key]['upper_limit']
-                if val<ll or val>ul:
+                if val < ll or val > ul:
                     raise ValueError('DAC value is out of limits')
                 else:
-                    orders.append(build_order([(1,8),(0,3*8),(pan,8),(chan,8),(float_to_uint16(val),16)]))
+                    orders.append(build_order([(1, 8), (0, 24), (pan, 8), (chan, 8), (float_to_uint16(val), 16)]))
         self.send_orders(orders)
         
     def read_current_values(self):
         """
         Asks the FPGA for the last values sent to each DAC output.
-        Returns a dict of type {DAC_key1:val1,DAC_key2:val2} and the content of the CIR7 memory.
+        Returns a dict of type {DAC_key1:val1, DAC_key2:val2} and the content of the CIR7 memory.
         """
         self.FPGA.acknowledge_irqs([3])
-        self.send_orders([build_order([(3,8),(0,7*8)])])
-        self.FPGA.wait_on_irqs([3],timeout_ms=10)
-        irq_status = self.FPGA.wait_on_irqs(3,timeout_ms=10)
+        self.send_orders([build_order([(3, 8), (0, 56)])])
+        self.FPGA.wait_on_irqs([3], timeout_ms=10)
+        irq_status = self.FPGA.wait_on_irqs(3, timeout_ms=10)
         if irq_status.timed_out:
             raise RuntimeError('Timeout while reading DAC values')
         else:
             DAC_val = self.FPGA.DAC_values.read()
             CIR7_val = self.FPGA.CIR7_values.read()
-            CIR7_val = [np.round(uint16_to_float(val),4) for val in CIR7_val]
+            CIR7_val = [np.round(uint16_to_float(val), 4) for val in CIR7_val]
             self.FPGA.acknowledge_irqs([3])
             outp_dict = {}
             for key in self.DAC_dict.keys():
-                pan = self.DAC_dict[key]['panel']
-                chan = self.DAC_dict[key]['channel']
-                outp_dict[key] = np.round(uint16_to_float(DAC_val[pan*8+chan]),4)
+                DAC_id = self.DAC_dict[key]['panel'] * 8 + self.DAC_dict[key]['channel']
+                outp_dict[key] = np.round(uint16_to_float(DAC_val[DAC_id]),4)
             return outp_dict, CIR7_val
+
+
+######################################
+###             SPI
+######################################
         
-    def SPI_read(self,address,Nbytes):
+    def SPI_read(self, address, Nbytes):
         """
-        Reads Nbytes from SPI.
+        Read Nbytes from SPI.
         """
-        self.send_orders([build_order([(2,8),(3,8),(0,8*3),(address,8),(Nbytes,16)])]) # read SPI
-        SPI_content = self.FPGA.SPI_data.read(Nbytes,timeout_ms=2000)
-        return [(d>>8,d%256) for d in SPI_content.data] # [(addr0,d0),(addr1,d1),...]
+        read_order = build_order([(2, 8), (3, 8), (0, 24), (address, 8), (Nbytes, 16)])
+        self.send_orders([read_order]) # read Nbytes from SPI
+        SPI_content = self.SPI_data.read(Nbytes, timeout_ms=2000) # read from memory content
+        if SPI_content.elements_remaining > 0:
+            raise RuntimeError('Found extra elements in SPI fifo')
+        elif [d >> 8 for d in SPI_content.data] != list(range(address, address + Nbytes)): # check addresses
+            raise RuntimeError('SPI content was incorrect')
+        return [d % 256 for d in SPI_content.data] # remove address flag, only return data
         
-    def SPI_write(self,address,data):
+    def SPI_write(self, address, data):
         """
-        Writes one or several byte(s) of data to the SPI.
+        Write one or several byte(s) of data to the SPI.
         """
-        if len(data)==0:
+        if len(data) == 0:
             data = [data]
         Nbytes = len(data)
         orders = []
-        for i,di in enumerate(data):
-            orders += [build_order([(2,8),(1,8),(0,8*4),(address+i,8),(di,8)])] # place in memory
-        orders += [build_order([(2,8),(2,8),(0,8*3),(address,8),(Nbytes,16)])] # write order
+        for (i, di) in enumerate(data):
+            orders += [build_order([(2, 8), (1, 8), (0, 32), (address + i, 8), (di, 8)])] # place in memory
+        orders += [build_order([(2, 8), (2, 8), (0, 24), (address, 8), (Nbytes, 16)])] # write order
         self.send_orders(orders)
+
+    def dump_all(self):
+        """
+        Prints whole SPI content.
+        """
+        print("Reg00 to Reg63", self.SPI_read(0x00, 64))
+        print("Min&Max addresses of loop counter", self.SPI_read(0x40, 2))
+        print("SRAM address, data & ctrl", self.SPI_read(0x50, 3))
+        print("Output buffers configuration", self.SPI_read(0x70, 4))
+        print("BIST clear", self.SPI_read(0x80, 1))
+        print("Modes and soft-reset", self.SPI_read(0xFE, 2))
+        print("Counter / Register / SRAM indexes", self.SPI_read(0xA0, 3))
+        print("Selected", self.SPI_read(0x90, 8))
         
-    def panel4_config(self,start_ind=0,stop_ind=63,ticks_per_address=800,ticks_per_bit=10):
+######################################
+###     PANEL4 AUTO-REFRESH
+######################################
+
+    def panel4_config(self, start_ind=0, stop_ind=63, ticks_per_address=800, ticks_per_bit=10):
         """
         Configures the refresh rate & communication speed of panel 4.
         """
-        if ticks_per_bit*39*2 > ticks_per_address:
+        if ticks_per_bit * 39 * 2 > ticks_per_address:
             raise ValueError('Refresh rate is too low')
-        orders = [build_order([(4,8),(3,8),(0,8),(0,8),(ticks_per_bit,32)])]
-        orders += [build_order([(4,8),(3,8),(0,8),(1,8),(ticks_per_address,32)])]
-        orders += [build_order([(4,8),(5,8),(0,8*4),(stop_ind,8),(start_ind,8)])]
-        orders += [build_order([(4,8),(2,8),(0,8*6)])] # start if not already running
+        orders = [build_order([(4, 8), (3, 8), (0, 8), (0, 8), (ticks_per_bit, 32)])]
+        orders += [build_order([(4, 8), (3, 8), (0, 8), (1, 8), (ticks_per_address, 32)])]
+        orders += [build_order([(4, 8), (5, 8), (0, 32), (stop_ind, 8), (start_ind, 8)])]
+        orders += [build_order([(4, 8), (2, 8), (0, 48)])] # start if not already running
         self.send_orders(orders)
         
-    def panel4_set_value(self,values_dict):
+    def panel4_set_value(self, values_dict):
         """
         Updates one or several programmed value(s) in CIR7.
         """
         orders = []
-        for key,val in values_dict.items():
-            if not isinstance(key,int) or key not in range(64):
+        for key, val in values_dict.items():
+            if not isinstance(key, int) or key not in range(64):
                 raise KeyError('Panel 4 key must be an integer from 0 to 63')
             else:
-                orders += [build_order([(4,8),(4,8),(0,8*3),(key,8),(float_to_uint16(val),16)])]
+                orders += [build_order([(4, 8), (4, 8), (0, 24), (key, 8), (float_to_uint16(val), 16)])]
         self.send_orders(orders)
         
     def panel4_stop(self):
         """
         Stops panel 4 refreshing
         """
-        stop_order = build_order([(4,8),(1,8),(0,8*6)])
+        stop_order = build_order([(4, 8), (1, 8), (0, 48)])
         self.send_orders([stop_order])
         
     def panel4_start(self):
         """
         Starts panel 4 refreshing
         """
-        stop_order = build_order([(4,8),(2,8),(0,8*6)])
+        stop_order = build_order([(4, 8), (2, 8), (0, 48)])
         self.send_orders([stop_order])
         
-    def config_seq(self,slots,us_per_DAC=1000,trig_reset_states=[True]*10,start_after=False,start_index=0):
+######################################
+###     FAST-SEQUENCE
+######################################
+
+    def config_seq(self, slots, us_per_DAC=1000, trig_reset_states=[True]*10, start_after=False, start_index=0):
         """
         Configures a fast sequence.
         slots is a dict of type {num:Slot_object}. (missing slots are not updated, use End slots to erase a previous sequence)
         """
-        stop_order = build_order([(5,8),(1,8),(0,6*8)])
-        trig_reset_code = sum([2**i for (i,b) in enumerate(trig_reset_states) if b])
-        trig_reset_order = build_order([(5,8),(10,8),(0,38),(trig_reset_code,10)])
-        us_DAC_order = build_order([(5,8),(8,8),(0,2*8),(us_per_DAC,32)])
-        orders = [stop_order,trig_reset_order,us_DAC_order]
+        stop_order = build_order([(5, 8), (1, 8), (0, 48)])
+        trig_reset_code = s = sum([b << i for (i, b) in enumerate(trig_reset_states)])
+        trig_reset_order = build_order([(5, 8), (10, 8), (0, 38), (trig_reset_code, 10)])
+        us_DAC_order = build_order([(5, 8), (8, 8), (0, 16), (us_per_DAC, 32)])
+        orders = [stop_order, trig_reset_order, us_DAC_order]
         # slots
-        for (i,slot) in slots.items():
-            if i not in range(0,1023):
+        for (i, slot) in slots.items():
+            if i not in range(0, 1023):
                 raise KeyError('Unknown slot id')
             else:
-                #print(i,slot.gen_order(i))
-                orders.append(slot.gen_order(i))
-                #print('{}:{:16X}'.format(i,orders[-1]))
+                orders += [build_order([(5, 8), (4, 8), (i, 8), (slot.gen_order(), 40)])]
+                print('{}:{:10X}'.format(i, slot.gen_order()))
         if start_after:
-            start_order = build_order([(5,8),(2,8),(0,38),(start_index,10)])
+            start_order = build_order([(5, 8), (2, 8), (0, 38), (start_index, 10)])
             orders.append(start_order)
         self.send_orders(orders)
 
-    def update_slots(self,slots,start_after=False,start_index=0):
+    def update_slots(self, slots, start_after=False, start_index=0):
         """
         Modifies several slots in the sequence.
         slots is a dict of type {num:Slot_object}
         start_after indicates whether to run the sequence or not after the modification.
-        e.g. update_slots({5:DAC_slot(0,0,-2),7:Timing_slot(1.0,'10us')})
+        e.g. update_slots({5:Wait(1.0, '10us'), 7:End()})
         """
-        start_order = build_order([(5,8),(2,8),(0,38),(start_index,10)])
-        stop_order = build_order([(5,8),(1,8),(0,6*8)])
+        stop_order = build_order([(5, 8), (1, 8), (0, 48)])
         orders = [stop_order]
-        for (i,slot) in slots.items():
-            if i not in range(0,1023):
+        for (i, slot) in slots.items():
+            if i not in range(0, 1023):
                 raise KeyError('Unknown slot id')
             else:
-                orders.append(slot.gen_order(i))
+                orders += [build_order([(5, 8), (4, 8), (i, 8), (slot.gen_order(), 40)])]
         if start_after:
+            start_order = build_order([(5, 8), (2, 8), (0, 38), (start_index, 10)])
             orders.append(start_order)
         self.send_orders(orders)
 
-    def update_slots_lowlevel(self,seq_orders,start_after=False,start_index=0):
+    def update_slots_lowlevel(self, seq_orders, start_after=False, start_index=0):
         """
         Modifies several slots in the sequence.
         seq_orders is a list of uint64s.
         start_after indicates whether to run the sequence or not after the modification.
         """
-        start_order = 0x0502<<6*8 | start_index
-        stop_order = 0x0501<<6*8
+        stop_order = build_order([(5, 8), (1, 8), (0, 48)])
         if start_after:
-            self.send_orders([stop_order,*seq_orders])
+            start_order = build_order([(5, 8), (2, 8), (0, 38), (start_index, 10)])
+            self.send_orders([stop_order, *seq_orders, start_order])
         else:
-            self.send_orders([stop_order,*seq_orders,start_order])
+            self.send_orders([stop_order, *seq_orders])
 
-    def start_seq(self,start_ind=0):
+    def start_seq(self, start_ind=0):
         """
         Runs the programmed sequence from the indicated row.
         """
-        self.send_orders([build_order([(5,8),(2,8),(0,38),(start_ind,10)])])
+        self.send_orders([build_order([(5, 8), (2, 8), (0, 38), (start_ind, 10)])])
 
     def stop_seq(self):
         """
         Stops the sequence execution.
         """
-        self.send_orders([build_order([(5,8),(1,8),(0,6*8)])])
+        self.send_orders([build_order([(5, 8), (1, 8), (0, 48)])])
 
-    def restart_seq(self,restart_ADC=False):
+    def restart_seq(self, restart_ADC=False):
         """
         Stops and runs the programmed sequence from the last starting index.
         restart_ADC allows to send the ADC start order immediately before (for timing performances)
         """
         if restart_ADC:
-            orders = [build_order([(6,8),(2,8),(0,6*8)])] # ADC start (wait for trigger)
+            orders = [build_order([(6, 8), (2, 8), (0, 48)])] # ADC start (wait for trigger)
         else:
             orders = []
-        orders.append(build_order([(5,8),(3,8),(0,6*8)])) # sequence restart
+        orders.append(build_order([(5, 8), (3, 8), (0, 48)])) # sequence restart
         self.send_orders(orders)
         
 if __name__=="__main__":
@@ -253,25 +291,21 @@ if __name__=="__main__":
     bitfile_path = """C:/Users/manip.batm/Documents/FPGA_Batch_2_0_5_CryoC009/Labview_2019/FPGA Bitfiles/FPGA_CIR7_main.lvbitx"""
     ip_address = "192.168.1.21"
     
-    instr = CIR7_driver(ip_address,bitfile_path,DAC_dict={})
+    instr = CIR7_driver(ip_address, bitfile_path, DAC_dict={})
     print(instr.read_current_values())
     
-    ans1 = instr.SPI_read(0x70,4)
-    print([hex(add)+' : '+hex(val) for (add,val) in ans1])
-    for (add,val) in ans1:
-        print(f"{add:02x}\t{val:02x}")
+    ans1 = instr.SPI_read(0x70, 4)
+    print('First SPI read : ')
+    for (i, val) in enumerate(ans1):
+        print(f"{0x70+i:02x}\t{val:02x}")
     
-    instr.SPI_write(0x70,[0x81,0x82,0x03,0x84])
+    instr.SPI_write(0x70, [0x81, 0x82, 0x03, 0x84])
+    print('Writing to SPI ...')
     time.sleep(0.1)
-    ans2 = instr.SPI_read(0x70,4)
-    for (add,val) in ans2:
-        print(f"{add:02x}\t{val:02x}")
-    
-    instr.panel4_config(start_ind=0,stop_ind=7,ticks_per_address=800,ticks_per_bit=10)
-    values_dict = {}
-    for addr in range(8):
-        values_dict[addr] = 0.1*addr
-    instr.panel4_set_value(values_dict)
+    ans2 = instr.SPI_read(0x70, 4)
+    print('Second SPI read : ')
+    for (i, val) in enumerate(ans1):
+        print(f"{0x70+i:02x}\t{val:02x}")
     
     instr.FPGA.close()
     
